d3b8f4f476443448db848854c6de362b7face6a9
annaco <conradsonanna@hotmail.com>
2018-04-23 22:42:20 +0200

    comments

--- src/js/main.js
+++ src/js/main.js
@@ -9,39 +9,28 @@ function counter() {
   
     var christmas_date = new Date('Mon Dec 24 2018 00:00:01 GMT +0100 (W. Europe Standard Time)');
 
-   
-  
     var christmas = christmas_date.getTime();
     var now = (new Date).getTime();
-       
-   
-
     var time_left = christmas - now;
 
-
+    //Uträkning av tid
     var seconds = time_left/1000;
-
-    
     var minutes = seconds/60;
-
-
     var hours = minutes/60;
 
+    //Antal dagar kavr
     var days = Math.floor(hours/24);
 
-    
+    //Antal timmar kavr
     var hours_left =  hours - (days*24);
-    
-    
     var hoursToChrismas =Math.floor(hours_left); 
 
+    //Antal minuter kvar
     var minutes_left = (hours_left-hoursToChrismas)*60;
-  
-
     var minutesToChristmas = Math.floor(minutes_left);
 
+    //Antal sekunder kvar
     var seconds_left = (minutes_left -  minutesToChristmas)*60;
-
     var secondsToChristmas = Math.floor(seconds_left);
 
    
